ab561ad363a31bd82a91c51af559aefdd6116ae1
vazgenvlasan <vazgo999@mail.ru>
2022-04-19 11:04:30 +0400

    java3_HW6

---
+++ src/main/java/HW6_3/Task3.java
@@ -0,0 +1,14 @@
+package HW6_3;
+
+
+import java.util.Arrays;
+
+public class Task3 {
+
+    public boolean examinationArray (int[] array) {
+        boolean check = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1 || array[i] == 4) check = true;
+        } return check;
+    }
+}
