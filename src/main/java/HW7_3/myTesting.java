132b22583d4305c95452689e63e6eb104ed3bc3a
vazgenvlasan <vazgo999@mail.ru>
2022-04-19 11:28:42 +0400

    java3_HW7

---
+++ src/main/java/HW7_3/myTesting.java
@@ -0,0 +1,10 @@
+package HW7_3;
+
+package q3Lesson7HomeWork.myTesting;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AfterSuite {
+}
---
+++ src/main/java/HW7_3/sdsds.java
@@ -0,0 +1,11 @@
+package HW7_3;
+
+package q3Lesson7HomeWork.myTesting;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+    int priority() default 1;
+}
